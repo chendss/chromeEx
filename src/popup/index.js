f71f2a63ad28297bb29a5ac769a18cebd1652375
dashao <capslock.ch@outlook.com>
2020-03-18 01:48:23 +0800

    拉钩

--- src/popup/index.js
+++ src/popup/index.js
@@ -1,55 +1,55 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import App from './App'
-import router from './router'
-// import VueAMap from 'vue-amap'
-// import ECharts from 'vue-echarts'
-// import VModal from 'vue-js-modal'
-import ElementUI from 'element-ui'
-// import mavonEditor from 'mavon-editor'
-// import VueVideoPlayer from 'vue-video-player'
-// import Vue2TouchEvents from 'vue2-touch-events'
-// import VueAwesomeSwiper from 'vue-awesome-swiper'
-// import VueVirtualScroller from 'vue-virtual-scroller'
+// import Vue from 'vue'
+// import Vuex from 'vuex'
+// import App from './App'
+// import router from './router'
+// // import VueAMap from 'vue-amap'
+// // import ECharts from 'vue-echarts'
+// // import VModal from 'vue-js-modal'
+// import ElementUI from 'element-ui'
+// // import mavonEditor from 'mavon-editor'
+// // import VueVideoPlayer from 'vue-video-player'
+// // import Vue2TouchEvents from 'vue2-touch-events'
+// // import VueAwesomeSwiper from 'vue-awesome-swiper'
+// // import VueVirtualScroller from 'vue-virtual-scroller'
 
-// import 'echarts/lib/chart/bar'
-// import 'video.js/dist/video-js.css'
-// import 'swiper/dist/css/swiper.css'
-// import 'echarts/lib/component/tooltip'
-// import 'mavon-editor/dist/css/index.css'
-import 'element-ui/lib/theme-chalk/index.css'
+// // import 'echarts/lib/chart/bar'
+// // import 'video.js/dist/video-js.css'
+// // import 'swiper/dist/css/swiper.css'
+// // import 'echarts/lib/component/tooltip'
+// // import 'mavon-editor/dist/css/index.css'
+// import 'element-ui/lib/theme-chalk/index.css'
 
-Vue.use(Vuex)
-// Vue.use(VModal)
-// Vue.use(VueAMap)
-Vue.use(ElementUI)
-// Vue.use(mavonEditor)
-// Vue.use(VueVideoPlayer)
-// Vue.use(Vue2TouchEvents)
-// Vue.use(VueAwesomeSwiper)
-// Vue.use(VueVirtualScroller)
+// Vue.use(Vuex)
+// // Vue.use(VModal)
+// // Vue.use(VueAMap)
+// Vue.use(ElementUI)
+// // Vue.use(mavonEditor)
+// // Vue.use(VueVideoPlayer)
+// // Vue.use(Vue2TouchEvents)
+// // Vue.use(VueAwesomeSwiper)
+// // Vue.use(VueVirtualScroller)
 
-// VueAMap.initAMapApiLoader({
-//   key: 'ea0a3afac3d92c5d88287f401d1ed076',
-//   plugin: [
-//     'AMap.Autocomplete',
-//     'AMap.PlaceSearch',
-//     'AMap.Scale',
-//     'AMap.OverView',
-//     'AMap.ToolBar',
-//     'AMap.MapType',
-//     'AMap.PolyEditor',
-//     'AMap.CircleEditor',
-//   ],
-//   // 高德 sdk 版本，默认为 1.4.4
-//   v: '1.4.4',
+// // VueAMap.initAMapApiLoader({
+// //   key: 'ea0a3afac3d92c5d88287f401d1ed076',
+// //   plugin: [
+// //     'AMap.Autocomplete',
+// //     'AMap.PlaceSearch',
+// //     'AMap.Scale',
+// //     'AMap.OverView',
+// //     'AMap.ToolBar',
+// //     'AMap.MapType',
+// //     'AMap.PolyEditor',
+// //     'AMap.CircleEditor',
+// //   ],
+// //   // 高德 sdk 版本，默认为 1.4.4
+// //   v: '1.4.4',
+// // })
+
+// // Vue.component('v-chart', ECharts)
+
+// new Vue({
+//   el: '#app',
+//   router,
+//   render: h => h(App),
+//   template: '<App/>',
 // })
-
-// Vue.component('v-chart', ECharts)
-
-new Vue({
-  el: '#app',
-  router,
-  render: h => h(App),
-  template: '<App/>',
-})
