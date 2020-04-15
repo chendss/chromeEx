e9f39a2734cff1cbf946f8f525b02a35adcbdeb9
dashao <capslock.ch@outlook.com>
2020-04-15 23:55:26 +0800

    排序

--- src/app/job/51job.js
+++ src/app/job/51job.js
@@ -1,8 +1,9 @@
-import { openLoading, closeLoading } from "@/utils"
-import { q, e } from "@/utils/tools"
-import GMap from '@/common/Map'
 import axios from 'axios'
+import GMap from '@/common/Map'
+import { q, e, es } from "@/utils/tools"
 import SearchFilter from './searchFilter'
+import { flatten } from 'lodash'
+import { openLoading, closeLoading } from "@/utils"
 
 const pageConfig = function () {
   const rtPrev = q('#rtPrev')
@@ -21,39 +22,84 @@ const nextPageUrl = function (nextIndex) {
   const location = window.location
   const { href } = location
   const index = globalConfig.index
-  const nextIndex = globalConfig.index
   const nextHref = href.replace(`${index}.html`, `${nextIndex}.html`)
   return nextHref
 }
 
-const onConfirm = function (data, ul) {
 
+
+const requestData = async function (index) {
+  const res = await axios.get(nextPageUrl(index), {
+    responseType: 'blob',
+    transformResponse: [
+      (data) => {
+        return new Promise((resolve) => {
+          let reader = new FileReader()
+          reader.readAsText(data, 'GBK')
+          reader.onload = function (e) {
+            resolve(reader.result)
+          }
+        })
+      }
+    ]
+  })
+  const data = await res.data
+  const p = new DOMParser()
+  const Html = p.parseFromString(data, "text/html")
+  const list = [...Html.querySelectorAll('#resultList .el:not(.title)')].filter(item => item.querySelector('.t4').innerText != '')
+  return list
+}
+
+const insertData = async function (ul) {
+  const { total, index } = pageConfig()
+  let promiseList = []
+  for (let i = (index + 1); i < total; i++) {
+    promiseList.push(requestData(i))
+  }
+  const res = await Promise.all(promiseList)
+  const domList = flatten(res)
+  domList.forEach(item => ul.appendChild(item))
+}
+
+const parsePrice = function (str) {
+  let result = []
+  if (str.includes('千')) {
+    let s = str.slice(0, str.length - 3)
+    result = s.split('-').map(i => Number(i))
+  } else {
+    let s = str.slice(0, str.length - 3)
+    result = s.split('-').map(i => 10 * Number(i))
+  }
+  if (str.includes('年')) {
+    result = result.map(i => Math.floor(i / 12))
+  }
+  return result
+}
+
+const init = function (ul) {
+  const list = es(ul, '.el:not(.title)')
+  for (let item of list) {
+    if (item.querySelector('.t4').innerText === '') {
+      item.classList.add('none')
+    } else {
+      const id = e(item, '.t1 input').value
+      const param = {
+        price: parsePrice(e(item, '.t4').innerText),
+        id
+      }
+      globalStore[id] = param
+    }
+  }
+}
+
+const onConfirm = function (data, ul) {
+  console.log('是多么', data)
 }
 
 const batchClick = function (ul) {
 
 }
 
-const requestData = function (index) {
-  const res = await axios.get(nextPageUrl(index))
-  const { data } = res
-  const p = new DOMParser()
-  const Html = p.parseFromString(data, "text/html")
-  const list = [...Html.querySelectorAll('#resultList .el')].filter(item => item.querySelector('.t4').innerText != '')
-  return list
-}
-
-const insertData = async function () {
-  const { total } = pageConfig()
-  let promiseList = []
-  for (let i = 0; i < total; i++) {
-    promiseList.push(requestData(i))
-  }
-  const res = await Promise.all(promiseList)
-  console.log('都是上面', res.length)
-}
-
-// https://search.51job.com/list/030200,000000,0000,00,9,99,亚马逊+运营,2,3.html?lang=c&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&ord_field=0&dibiaoid=0&line=&welfare=
 
 export default async function () {
   openLoading()
@@ -64,6 +110,7 @@ export default async function () {
     onConfirm: (data) => onConfirm(data, ul),
     batchClick: (data) => batchClick(ul),
   })
-  await insertData()
+  await insertData(ul)
+  init(ul)
   closeLoading()
 }
