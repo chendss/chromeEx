c45f7591d9b0909ca29cb4d4554ab01b9e8f63a2
chen.yulin <zudao.cyl@dtyunxi.com>
2020-06-11 18:27:11 +0800

    修正

--- src/app/global/copyHtml.js
+++ src/app/global/copyHtml.js
@@ -1,13 +1,26 @@
 import Html from './index.html'
 import { q } from '@/utils/tools'
 import axios from 'axios'
+import { textToDom } from '../../utils'
 
 const btnClick = async function () {
-  const html = document.documentElement.outerHTML
+  const context = document.documentElement.outerHTML
+  const doc = textToDom(context)
+  const scriptHtml = `<script>
+    window.onbeforeunload = function () {
+      return confirm('你确定要关闭吗？')
+    }
+  </script>`
+  doc.head.insertAdjacentHTML('afterbegin', scriptHtml)
+  const html = doc.documentElement.outerHTML
   const url = 'http://127.0.0.1:9080/copy_html'
   const title = document.title
-  const res = await axios.post(url, { html, title })
-  console.log('是这样吗', res)
+  const href = window.location.href
+  const res = await axios.post(url, { html, title, href }, { headers: { 'Content-Type': 'application/json' } })
+  const code = get(res, 'data.code', null)
+  if (code === '0') {
+    alert('复刻成功')
+  }
 }
 
 export default function () {
