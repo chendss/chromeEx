da3e9f033e937da45920e4c7a6304eb77924ff8f
dashao <capslock.ch@outlook.com>
2020-06-24 01:31:12 +0800

    修复bug

--- src/app/global/copyHtml.js
+++ src/app/global/copyHtml.js
@@ -1,20 +1,28 @@
 import Html from './index.html'
+import './styles/copy.scss'
 import Config from '../../assets/custom'
 import { q } from '@/utils/tools'
 import axios from 'axios'
 import { textToDom, get } from '../../utils'
 
 const postData = async function (url_) {
-  const context = document.documentElement.outerHTML
-  const doc = textToDom(context)
-  const html = doc.documentElement.outerHTML
-  const url = url_ || (Config().copyUrl + '/copy_html')
-  const href = window.location.href
-  const res = await axios.post(url, { html, href, cover, description }, { headers: { 'Content-Type': 'application/json' } })
-  const code = get(res, 'data.code', null)
-  if (code === '0') {
-    alert('复刻成功')
+  const btn = q('#id-copy-btn')
+  btn.setAttribute('loading', 'true')
+  try {
+    const context = document.documentElement.outerHTML
+    const doc = textToDom(context)
+    const html = doc.documentElement.outerHTML
+    const url = url_ || (Config().copyUrl + '/copy_html')
+    const href = window.location.href
+    const res = await axios.post(url, { html, href }, { headers: { 'Content-Type': 'application/json' } })
+    const code = get(res, 'data.code', null)
+    if (code === '0') {
+      alert('复刻成功')
+    }
+  } catch (error) {
+    console.log('抛出异常', error)
   }
+  btn.setAttribute('loading', 'false')
 }
 
 const btnClick = async function () {
